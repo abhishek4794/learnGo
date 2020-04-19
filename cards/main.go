394b84049795409a62fbdf173f85dfc2d12971ca
Abhishek Marathe <abhishekyogesh.marathe@team.telstra.com>
2020-04-19 12:25:33 +0530

    First commit, Add basic go commands

---
+++ cards/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	cards := newDeck()
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
+	hand, remainingDeck := deal(cards, 5)
+	hand.print()
+	remainingDeck.print()
+	deckString := cards.toString()
+	fmt.Println(deckString)
+	cards.saveToFile("cards.csv")
+	newDeck := newDeckFromFile("cards.csv")
+	newDeck.print()
+	//cards.print()
+}
